4aa6b1e2402eee7c38f5f78b028026289f470c23
Rakiiii <sudaru2@gmail.com>
2018-02-16 19:00:05 +0300

    ref

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/SmurfIOLib.c.obj"
+  "libuntitled.pdb"
+  "libuntitled.dll"
+  "libuntitled.dll.a"
+  "libuntitled.dll.manifest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
